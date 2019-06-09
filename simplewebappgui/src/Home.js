080a0057e9c3ee67d5ccc27996ae7dbe5d14cc26
natasha yuhimuk <32952394+natashayuhimuk@users.noreply.github.com>
2019-06-09 19:43:59 +0300

    update home

--- simplewebappgui/src/Home.js
+++ simplewebappgui/src/Home.js
@@ -10,7 +10,7 @@ class Home extends Component{
             <div>
                 <AppNavbar/>
                 <Container fluid>
-                    <Button color="link"><Link to="/employees"> Manage Employee </Link></Button>
+                    <Button color="link"><Link to="/employees"> Employee management </Link></Button>
                 </Container>
             </div>
         );
